5a1a72c42ba5c74060c52477d746ba8d6630a107
Zectalia <95757705+Zectalia@users.noreply.github.com>
2022-11-14 13:02:25 +0100

    Etat bloqué (SpawnBlocks)

--- 2048 Copy/Assets/Scripts/Block.cs
+++ 2048 Copy/Assets/Scripts/Block.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static GameManager;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Value;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private TextMeshPro _text;
+    public void Init(BlockType type)
     {
-        
+        Value = type.Value;
+        _renderer.color = type.Color;
+        _text.text = type.Value.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
