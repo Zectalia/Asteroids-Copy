d30610cd286b9d61a5e43921e0cb82626695f5f6
Zectalia <95757705+Zectalia@users.noreply.github.com>
2022-11-18 09:58:36 +0100

    Etat final 3 ?

---
+++ Asteroids/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MovetoScene(int SceneID)
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Asteroids/Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public ParticleSystem explosionEffect;
+    public GameObject gameOverUI;
+    public GameObject ColSFX;
+
+    public int score { get; private set; }
+    public Text scoreText;
+
+    public int lives { get; private set; }
+    public Text livesText;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (lives <= 0 && Input.GetKeyDown(KeyCode.Return)) {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        Asteroid[] asteroids = FindObjectsOfType<Asteroid>();
+
+        for (int i = 0; i < asteroids.Length; i++) {
+            Destroy(asteroids[i].gameObject);
+        }
+
+        gameOverUI.SetActive(false);
+
+        SetScore(0);
+        SetLives(3);
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        player.transform.position = Vector3.zero;
+        player.gameObject.SetActive(true);
+    }
+
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        explosionEffect.transform.position = asteroid.transform.position;
+        explosionEffect.Play();
+        Instantiate(ColSFX, transform.position, transform.rotation);
+
+        if (asteroid.size < 0.7f) {
+            SetScore(score + 100); 
+        } else if (asteroid.size < 1.4f) {
+            SetScore(score + 50); 
+        } else {
+            SetScore(score + 25); 
+        }
+    }
+
+    public void PlayerDeath(Player player)
+    {
+        explosionEffect.transform.position = player.transform.position;
+        explosionEffect.Play();
+        Instantiate(ColSFX, transform.position, transform.rotation);
+
+        SetLives(lives - 1);
+
+        if (lives <= 0) {
+            GameOver();
+        } else {
+            Invoke(nameof(Respawn), player.respawnDelay);
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        livesText.text = lives.ToString();
+    }
+
+}
